258cf7d9e18a45db26b3b9143787210c57bef830
Ahmad M <ahmadmoawad2@yahoo.com>
2020-04-19 18:48:18 +0200

    add bag

---
+++ src/main/java/com/alg/fundamentals/Stopwatch.java
@@ -0,0 +1,24 @@
+package com.alg.fundamentals;
+
+public final class Stopwatch {
+
+    private static Long start;
+
+    private Stopwatch() {
+        throw new IllegalAccessError("Cannot create object from this class!");
+    }
+
+    public final static void start() {
+        start = System.currentTimeMillis();
+    }
+
+    public final static double elapsedTime() {
+        if (start == null) {
+            throw new IllegalAccessError("Please call start function before elapsedTime");
+        }
+        long now = System.currentTimeMillis();
+        double result = (now - start) / 1000.0;
+        start = null;
+        return result;
+    }
+}
